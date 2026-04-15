3f719a24cb5a65511aef2b134659b5348a1b9eb4
devmail8181@gmail.com <devmail8181@gmail.com>
2026-04-15 11:44:04 -0600

    More logs

--- HexArmory/Patches/ObjectDbPatch.cs
+++ HexArmory/Patches/ObjectDbPatch.cs
@@ -1,5 +1,7 @@
 ﻿using HarmonyLib;
+using HarmonyLib;
 using HexArmory.Core;
+using UnityEngine;
 using System;
 
 namespace HexArmory.Patches
@@ -42,6 +44,37 @@ namespace HexArmory.Patches
 
                 HexContentBuilder.BuildAll(objectDb);
                 HexLifecycleRegistrar.RegisterAll(objectDb);
+
+                // Diagnostics: log available PieceTables and CraftingStations to help identify station/piece prefab names
+                try
+                {
+                    var pieceTables = Resources.FindObjectsOfTypeAll<PieceTable>();
+                    foreach (var pt in pieceTables)
+                    {
+                        Plugin.Log.LogInfo($"PieceTable found: name='{pt.name}', pieces={pt.m_pieces?.Count ?? 0}");
+                        if (pt.m_pieces != null)
+                        {
+                            foreach (var go in pt.m_pieces)
+                            {
+                                if (go == null) continue;
+                                var pieceComp = go.GetComponent<Piece>();
+                                string pieceName = pieceComp != null ? pieceComp.m_name : "<no Piece>";
+                                string prefabName = go.name;
+                                Plugin.Log.LogInfo($"  piece prefab='{prefabName}', gameObj='{go.name}', Piece.m_name='{pieceName}'");
+                            }
+                        }
+                    }
+
+                    var stations = Resources.FindObjectsOfTypeAll<CraftingStation>();
+                    foreach (var cs in stations)
+                    {
+                        Plugin.Log.LogInfo($"CraftingStation found: name='{cs.name}', gameObj='{cs.gameObject?.name}', type='{cs.GetType().FullName}'");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Plugin.Log.LogWarning("HexArmory station/piece diagnostics failed: " + ex);
+                }
 
                 Plugin.Log.LogInfo(source + ": HexArmory ObjectDB registration finished.");
             }
